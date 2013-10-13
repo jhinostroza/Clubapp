5fd946e9269503cbc0aee3768648ac555ac7b5eb
LAB704-00 <LAB704-00@LAB704-06>
2013-10-13 11:30:43 -0500

    iniciando

---
+++ src/main/java/app/model/Local.java
@@ -0,0 +1,63 @@
+package app.model;
+
+public class Local {
+    
+    private long idLocal;
+    private String direccion;
+    private String descripcion;
+    private int estado;
+    private String maps;
+    private String telefono;
+
+    public long getIdLocal() {
+        return idLocal;
+    }
+
+    public void setIdLocal(long idLocal) {
+        this.idLocal = idLocal;
+    }
+   
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getMaps() {
+        return maps;
+    }
+
+    public void setMaps(String maps) {
+        this.maps = maps;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
